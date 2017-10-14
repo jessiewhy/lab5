ab317481ca3b7c839cba431c217b759f79d48b66
Jessie <jessiemh@ufl.edu>
2017-10-14 10:54:24 -0400

    v3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,31 +3,19 @@ import java.util.*;
 
 
 public class Main {
-    public static int hex2Decimal(String s) {
-        String digits = "0123456789ABCDEF";
-        s = s.toUpperCase();
+    public static int hex2Decimal(String j) {
+        String hexMap = "0123456789ABCDEF";
+        j = j.toUpperCase();
         int val = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            int d = digits.indexOf(c);
-            val = 16*val + d;
+        for (int i = 0; i < j.length(); i++) {
+            char c = j.charAt(i);
+            int d = hexMap.indexOf(c);
+            if (d != -1) {
+                val = 16 * val + d;
+            }
         }
         return val;
     }
-
-
-    // precondition:  d is a nonnegative integer
-    /*public static String decimal2Hex(int d) {
-        String digits = "0123456789ABCDEF";
-        if (d == 0) return "0";
-        String hex = "";
-        while (d > 0) {
-            int digit = d % 16;                // rightmost digit
-            hex = digits.charAt(digit) + hex;  // string concatenation
-            d = d / 16;
-        }
-        return hex;
-    }*/
     public static void main(String[] args) {
         int decimal = hex2Decimal(args[0]);
         System.out.println(decimal);
